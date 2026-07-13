2ba9247faf29bf76bfa894c6c94c97f196266a8f
Radokaz <radokazgames@gmail.com>
2026-07-12 23:02:39 -0300

    Bug no DMA corrigido.

--- build_windows/include/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/radokaz/Trabalho de metodologia/Emulador/include/raylib/src
-# Build directory: /home/radokaz/Trabalho de metodologia/Emulador/build_windows/include/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
