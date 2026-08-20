84bce2d74ca455eaeed81f2aeb4c8a83ce18bd3b
Radokaz <radokazgames@gmail.com>
2026-08-19 21:37:35 -0300

    Correção no mesa uwp.

---
+++ include/mesa-uwp/include/uwp_util.h
@@ -0,0 +1,16 @@
+#ifndef UWP_UTIL_H
+#define UWP_UTIL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void* uwp_get_window_reference(void);
+int uwp_get_height(void);
+int uwp_get_width(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
